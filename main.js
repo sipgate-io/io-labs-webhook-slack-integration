480b5471b7ac0a86aa41804bbd669563d1e758b1
Björn Vopel <vopel@sipgate.de>
2020-07-22 10:32:14 +0200

    feat: add error handling
    Co-Authored-By: Max Massing <massing@sipgate.de>
    Co-Authored-By: David Borzek <borzek@sipgate.de>

--- main.js
+++ main.js
@@ -1,23 +1,42 @@
 const { createWebhookModule } = require("sipgateio");
 const axios = require("axios").default;
 
+const slackWebhookUrl = process.env.SLACK_WEBHOOK_URL;
+if (!slackWebhookUrl) {
+  console.error(
+    "Please provide a Slack webhook URL via the environment variable SLACK_WEBHOOK_URL"
+  );
+  return;
+}
+
+const serverAddress = process.env.SERVER_ADDRESS;
+if (!serverAddress) {
+  console.error(
+    "Please provide a server address via the environment variable SERVER_ADDRESS"
+  );
+  return;
+}
+
 const webhookServerOptions = {
   port: process.env.SERVER_PORT || 8080,
-  serverAddress: process.env.SERVER_ADDRESS,
+  serverAddress,
 };
 
 createWebhookModule()
   .createServer(webhookServerOptions)
   .then((server) => {
     console.log(
-      `Server running at ${webhookServerOptions.serverAddress}\n` + "Ready for calls 📞"
+      `Server running at ${webhookServerOptions.serverAddress}\n` +
+        "Ready for calls 📞"
     );
 
     server.onHangUp((hangUpEvent) => {
       if (hangUpEvent.cause === "cancel") {
-        axios.post(process.env.SLACK_WEBHOOK_URL, {
-          text: `Cancelled call from ${hangUpEvent.from} to ${hangUpEvent.to}`,
-        });
+        axios
+          .post(slackWebhookUrl, {
+            text: `Cancelled call from ${hangUpEvent.from} to ${hangUpEvent.to}`,
+          })
+          .catch(console.error);
       }
     });
   });
